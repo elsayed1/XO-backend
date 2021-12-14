4f0c0b901dec76c0993bfacfa68ed50f35ff4591
elsayed1 <elsayed0alaa@gmail.com>
2021-12-14 22:21:43 +0200

    solve connecetion issues

--- index.js
+++ index.js
@@ -1,65 +1,77 @@
-const express = require("express");
-const app = express();
-const server = require("http").Server(app);
+// const express = require("express");
+// const app = express();
+// const port = process.env.PORT || 3000;
+// const server = app.listen(port, () => console.log("server is runnig on port" + port));
 
-const io = require("socket.io")(server);
+// const io = require("socket.io")(server);
+
+const http = require("http");
+const express = require("express");
+const socketIO = require("socket.io");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+server.listen(PORT, () => {
+  console.log("server started and listening on port " + PORT);
+});
 
 app.get("/", (req, res) => {
   res.end("Hello");
 });
 let rooms = 0;
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
   console.log("user connected ", socket.id);
+
   // Create a new game room and notify the creator of game.
-  socket.on("create room", player1 => {
+  socket.on("create room", (player1) => {
     socket.join(`room${++rooms}`);
     socket.emit("room created", {
       player1,
-      roomName: `room${rooms}` 
+      roomName: `room${rooms}`,
     });
-    socket.player=player1
+    socket.player = player1;
     // console.log(io.nsps["/"].adapter.rooms[`room1`].sockets)
     //console.log(io.sockets.connected[socket.id].player)
   });
 
   // Connect the Player 2 to the room he requested. Show error if room full.
-  socket.on("join room", ({roomName,player2}) => {
-    var room = io.nsps["/"].adapter.rooms[roomName];
-    if (room && room.length === 1) {
+  socket.on("join room", ({ roomName, player2 }) => {
+    var room = io.sockets.adapter.rooms.get(roomName);
+    if (room && room.size >= 1) {
+
       socket.join(roomName);
-      socket.broadcast
-        .to(roomName)
-        .emit("player2 joined", { player2 });
+      io.to(roomName).emit("player2 joined", { player2,roomName });
     } else {
       socket.emit("err", {
-        message: "Sorry, The room is full!"
+        message: "Sorry, The room is full!",
       });
     }
   });
 
   // return the player1 name to the player2
-  socket.on("player1 name", ({ player1, roomName }) => {
-    socket.broadcast.to(roomName).emit("player1 name", { player1 });
+  socket.on("cast player1", ({ roomName,player1 }) => {
+    console.log(player1)
+    io.to(roomName).emit("player1 name", { player1 });
   });
 
-
-
   // Handle the turn played by either player and notify the other.
-  socket.on("playTurn", data => {
+  socket.on("playTurn", (data) => {
     socket.broadcast.to(data.room).emit("turnPlayed", {
       tile: data.tile,
-      room: data.room
+      room: data.room,
     });
   });
 
   /**
    * Notify the players about the victor.
    */
-  socket.on("gameEnded", data => {
+  socket.on("gameEnded", (data) => {
     socket.broadcast.to(data.room).emit("gameEnd", data);
   });
-
 });
-const port = process.env.PORT || 3000;
-server.listen(port, () => console.log("server is runnig on port" + port));
